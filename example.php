31d23ce1f8d5510b51e07c47e7cb49421bcac061
Philip Whisenhunt <philip.whisenhunt@gmail.com>
2019-01-25 10:27:26 -0500

    checking in rest of files

---
+++ example.php
@@ -0,0 +1,23 @@
+<?php
+
+//Requirements: 
+/*
+Make an API
+
+Going to need a GET and POST route
+
+The POST route needs to:
+accept JSON
+    should end up containing a persons first name and last name.
+
+If they post something that is not first name [or last name], respond with a 400 error.
+
+Otherwise store the name in a csv file. 
+
+The get request should return the names in the CSV
+    if there is no name or no CSV, return a 404 error.
+
+    error needs to say why it failed, say what was missing, in JSON. 
+
+    */
+
---
+++ index.php
@@ -0,0 +1,77 @@
+<?php
+
+// echo "hello";
+
+//handle whether or not its a get or post 
+// $name = "land";
+
+
+if($_SERVER['REQUEST_METHOD'] === 'POST'){
+    // echo "its a post";
+
+    //what does this line do?
+    //My guess is that 
+    $content = json_decode(file_get_contents("php://input"), true);
+    print_r($content);
+
+    if($content["firstName"]){
+        //write to CSV
+
+        //open the file and make it writeable
+        $file = fopen("apiExample.csv","a");
+       
+
+        //name the file
+        $firstName = $content['firstName'];
+        $lastName = $content['lastName'];
+
+
+        //write to the file
+        fputcsv($file, explode(',', $firstName));
+
+        //close the file
+        fclose($file);
+
+    
+    }
+
+    // if($content["lastName"]){
+
+    //     $file = fopen("apiExample.csv", "w");
+
+    //     $lastName = $content['lastName'];
+
+    //     fputcsv($file, explode(',', $lastName));
+
+    //     fclose($file);
+    // }
+    
+    else{
+        //respond with 400 error
+        http_response_code(400);
+        //explain why
+
+    }
+}
+
+
+
+if($_SERVER['REQUEST_METHOD'] === 'GET'){
+
+    if(file_exists("apiExample.csv")){
+        //return the names in the csv file
+        $file = fopen("apiExample.csv","r");
+        $returnedArray = fgetcsv($file);
+
+        //respond with the entries under name
+        print_r($returnedArray);
+        fclose($file);
+
+    }
+
+    else{
+        //return 404 error
+        http_response_code(404);
+
+    }
+}
